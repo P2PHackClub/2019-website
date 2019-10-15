2522322c6fd03c3859ba3d6a4b3de91fb6389db7
James Kerrane <jamesjpkerrane@gmail.com>
2019-10-14 20:28:09 -0600

    Hello World!
    First major version of the P2P Hack Club Website, with logo, description, and a section with the next meeting.
    
    Code is very bad, will refactor once the full design is here.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,67 @@
 import React from "react"
+import { ThemeProvider, Box, Link, Image, Heading, Card, Icon, Text, Flex } from '@hackclub/design-system'
+import styled from 'styled-components'
 
-export default () => <div>Hello world!</div>
+export default () => (
+
+<ThemeProvider webfonts>
+
+<Box align="center" py={5}>
+    <Link href="https://hackclub.com">
+      <Image
+        src="/logo.svg"
+        width={200}
+        mx="auto"
+        alt="P2P Hack Club Logo"
+      />
+      </Link>
+</Box>
+
+<Heading m={3} align="center">
+P2P Hack Club
+</Heading>
+
+<Subtitle m={3} align="center">
+A student run middle-school club at Peak to Peak Charter school where we build amazing things. Future meetings are expected on Thursdays. For more information, you can talk to Reema, Siddharth, Arihant, or JP.
+</Subtitle>
+
+<CardGroup>
+<Card
+  boxShadowSize="sm"
+  my={4}
+  p={3}
+  color="black"
+  bg="white"
+>
+
+<div style={{ color: '#A87EEA' }}>
+    <Icon glyph="event-code" size={64}/>
+    <Text m={0.5} bold>
+      Next Meeting
+    </Text>
+    <Text>
+    October 17th from 3:15-4:00pm in S297.
+    </Text>
+  </div>
+
+</Card>
+</CardGroup>
+
+</ThemeProvider>
+
+)
+
+const CardGroup = styled(Flex)`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  flex-wrap: wrap;
+
+`
+
+const Subtitle = styled(Text)`
+  max-width: 500px
+  text-align: center;
+  margin:0 auto;
+`
